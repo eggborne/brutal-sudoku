72bd38b0fb00f19e699746cd9300d19ce3ec5038
Mike Donovan <mike@mikedonovan.dev>
2023-01-31 11:58:59 -0800

    Add rotateMatrix and checkMatrixForUniqueness

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,3 +15,79 @@ describe('SudokuChecker.prototype.checkArrayForUniqueness', () => {
     expect(sudokuChecker.checkArrayForUniqueness(incorrectTestArray)).toEqual(false);
   });
 });
+
+describe('SudokuChecker.prototype.rotateMatrix', () => {
+
+  let sudokuChecker, testMatrix, rotatedTestMatrix;
+
+  beforeEach(() => {
+    sudokuChecker = new SudokuChecker();
+    testMatrix = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9],
+    ];
+    rotatedTestMatrix = [
+      [1,4,7],
+      [2,5,8],
+      [3,6,9],
+    ];
+  });
+
+  test('should return a 2d array whose rows equal the columns of the input array', () => {
+    expect(sudokuChecker.rotateMatrix(testMatrix)).toEqual(rotatedTestMatrix);
+  });
+});
+
+describe('SudokuChecker.prototype.checkMatrixForUniqueness', () => {
+
+  let sudokuChecker, correctTestMatrix, incorrectTestMatrix, incorrectColumnOnlyTestMatrix;
+
+  beforeEach(() => {
+    sudokuChecker = new SudokuChecker();
+    correctTestMatrix = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [4,5,6,7,8,9,1,2,3],
+      [5,6,7,8,9,1,2,3,4],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+    incorrectTestMatrix = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [4,5,6,7,8,9,1,3,3],
+      [5,6,7,8,9,1,2,3,4],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+    incorrectColumnOnlyTestMatrix = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [5,6,7,8,9,1,2,3,4],
+      [4,5,6,7,8,9,1,3,2],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+  });
+
+  test('should correctly determine if all of a matrix\'s rows have unique digits 1-9', () => {
+    expect(sudokuChecker.checkMatrixForUniqueness(correctTestMatrix)).toEqual(true);
+    expect(sudokuChecker.checkMatrixForUniqueness(incorrectTestMatrix)).toEqual(false);
+  });
+  
+  test('should correctly determine if all of a matrix\'s columns have unique digits 1-9', () => {
+    // expect(sudokuChecker.checkMatrixForUniqueness(correctTestMatrix)).toEqual(true);
+    // expect(sudokuChecker.checkMatrixForUniqueness(incorrectTestMatrix)).toEqual(false);
+    expect(sudokuChecker.checkMatrixForUniqueness(incorrectColumnOnlyTestMatrix)).toEqual(false);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -5,7 +5,40 @@ export class SudokuChecker {
 
   }
 
-  checkArrayForUniqueness(arr) {    
+  checkArrayForUniqueness(arr) {
     return [...arr].sort().join() === '1,2,3,4,5,6,7,8,9';
+  }
+
+  checkMatrixForUniqueness(matrix) {
+    let unique = true;
+    let columns = this.rotateMatrix(matrix);
+    for (const rowIndex in matrix) {
+      let row = matrix[rowIndex];
+      if (!this.checkArrayForUniqueness(row)) {
+        unique = false;
+        break;
+      }
+    }
+    for (const rowIndex in columns) {
+      let row = columns[rowIndex];
+      if (!this.checkArrayForUniqueness(row)) {
+        unique = false;
+        break;
+      }
+    }
+    return unique;
+  }
+
+  rotateMatrix(matrix) {
+    let newMatrix = [];
+    for (let i=0; i<matrix.length; i++) {
+      newMatrix.push([]);
+    }
+    matrix.forEach((row, r) => {
+      row.forEach((digit, c) => {
+        newMatrix[c][r] = digit;
+      });
+    });
+    return newMatrix;
   }
 }
