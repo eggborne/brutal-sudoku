d1b3d1c38f1f91acad355d8a73bb49ac797ca43a
Mike Donovan <mike@mikedonovan.dev>
2023-01-31 16:41:58 -0800

    Add getSquareRootMatrices (WIP)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -86,8 +86,126 @@ describe('SudokuChecker.prototype.checkMatrixForUniqueness', () => {
   });
   
   test('should correctly determine if all of a matrix\'s columns have unique digits 1-9', () => {
-    // expect(sudokuChecker.checkMatrixForUniqueness(correctTestMatrix)).toEqual(true);
-    // expect(sudokuChecker.checkMatrixForUniqueness(incorrectTestMatrix)).toEqual(false);
+    expect(sudokuChecker.checkMatrixForUniqueness(correctTestMatrix)).toEqual(true);
+    expect(sudokuChecker.checkMatrixForUniqueness(incorrectTestMatrix)).toEqual(false);
     expect(sudokuChecker.checkMatrixForUniqueness(incorrectColumnOnlyTestMatrix)).toEqual(false);
   });
+});
+
+describe('SudokuChecker.prototype.getSquareRootMatrices', () => {
+
+  let sudokuChecker, testMatrix, squareRootMatricesArray;
+
+  beforeEach(() => {
+    sudokuChecker = new SudokuChecker();
+    testMatrix = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [4,5,6,7,8,9,1,2,3],
+      [5,6,7,8,9,1,2,3,4],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+    squareRootMatricesArray = [
+      [
+        [1,2,3],
+        [2,3,4],
+        [3,4,5],
+      ],
+      [
+        [4,5,6],
+        [5,6,7],
+        [6,7,8],
+      ],
+      [
+        [7,8,9],
+        [8,9,1],
+        [9,1,2],
+      ],
+      [
+        [4,5,6],
+        [5,6,7],
+        [6,7,8],
+      ],
+      [
+        [7,8,9],
+        [8,9,1],
+        [9,1,2],
+      ],
+      [
+        [1,2,3],
+        [2,3,4],
+        [3,4,5],
+      ],
+      [
+        [7,8,9],
+        [8,9,1],
+        [9,1,2],
+      ],
+      [
+        [1,2,3],
+        [2,3,4],
+        [3,4,5],
+      ],
+      [
+        [4,5,6],
+        [5,6,7],
+        [6,7,8],
+      ],
+    ]
+  });
+
+  test('should convert a 2d array into an array of equally-sized square matrices', () => {
+    let splitMatrix = sudokuChecker.getSquareRootMatrices(testMatrix);
+    let numberOfMinis = splitMatrix.length;
+    let minisContainThreeArraysOfLengthThree = true;
+    splitMatrix.forEach(mini => {
+      if (mini.length !== 3 || mini[0].length !== 3 || mini[1].length !== 3 || mini[2].length !== 3) {
+        minisContainThreeArraysOfLengthThree = true;
+      }
+    })
+    expect(numberOfMinis).toEqual(9);
+    expect(minisContainThreeArraysOfLengthThree).toEqual(true);
+  });
+
+  test('should convert a 2d array into an array of square matrices which contain only numbers', () => {
+    let notAllNumbersMatrix = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [4,5,6,7,'k',9,1,2,3],
+      [5,6,7,8,9,1,2,3,4],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+    let splitMatrix = sudokuChecker.getSquareRootMatrices(testMatrix);
+    let splitIncorrectMatrix = sudokuChecker.getSquareRootMatrices(notAllNumbersMatrix);
+    let allNumbers = false;
+    let nonNumbersDetected = false;
+    splitMatrix.forEach(mini => {
+      mini.forEach(row => {
+        if (row.filter(digit => isNaN(digit)).length > 0) {
+          allNumbers = false;
+        }
+      });
+    })
+    splitIncorrectMatrix.forEach(mini => {
+      mini.forEach(row => {
+        if (row.filter(digit => isNaN(digit)).length > 0) {
+          nonNumbersDetected = true;
+        }
+      });
+    })
+    expect(allNumbers).toEqual(true);
+    expect(nonNumbersDetected).toEqual(true);
+  });
+
+  // test('should convert a 2d array into an array of equally-sized square matrices whose contents properly match the original', () => {
+  //   expect(sudokuChecker.getSquareRootMatrices(testMatrix)).toEqual(squareRootMatricesArray);
+  // });
 });
--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ export class SudokuChecker {
     return [...arr].sort().join() === '1,2,3,4,5,6,7,8,9';
   }
 
+  rotateMatrix(matrix) {
+    let newMatrix = [];
+    for (let i=0; i<matrix.length; i++) {
+      newMatrix.push([]);
+    }
+    matrix.forEach((row, r) => {
+      row.forEach((digit, c) => {
+        newMatrix[c][r] = digit;
+      });
+    });
+    return newMatrix;
+  }
+
   checkMatrixForUniqueness(matrix) {
     let unique = true;
     let columns = this.rotateMatrix(matrix);
@@ -29,16 +42,42 @@ export class SudokuChecker {
     return unique;
   }
 
-  rotateMatrix(matrix) {
-    let newMatrix = [];
-    for (let i=0; i<matrix.length; i++) {
-      newMatrix.push([]);
+  getSquareRootMatrices(matrix) {
+    let matrices = [];
+    let originalSize = matrix[0].length;
+    let sqrt = Math.sqrt(originalSize);
+    for (let i=0; i<originalSize; i++) {
+      matrices[i] = [];
+      for (let j=0; j<sqrt; j++) {
+        matrices[i].push([]);
+      }
     }
-    matrix.forEach((row, r) => {
-      row.forEach((digit, c) => {
-        newMatrix[c][r] = digit;
-      });
+    let flatMatrix = matrix.flat(2);
+    let currentMiniIndex = 0;
+    let currentMiniRow = 0;
+    let currentMiniColumn = 0;
+    flatMatrix.forEach((digit, d) => {
+      
+      // console.log('mini index', currentMiniIndex)
+      // console.log('row', currentMiniRow)
+      // console.log('col', currentMiniColumn)
+      matrices[currentMiniIndex][currentMiniRow][currentMiniColumn] = digit;
+      if (currentMiniColumn + 1 === sqrt) {
+        currentMiniColumn = 0;
+        if (currentMiniRow + 1 === sqrt) {
+          currentMiniRow = 0;
+        } else {
+          currentMiniRow++;
+        }
+      } else {
+        currentMiniColumn++;
+      }
+      if (d > 0 && d % originalSize === 0 && (currentMiniIndex+1) < originalSize) {
+        currentMiniIndex++;
+      }
     });
-    return newMatrix;
+    console.log(matrices);
+
+    return matrices;
   }
 }
